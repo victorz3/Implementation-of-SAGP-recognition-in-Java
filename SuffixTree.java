cb9c7cdffa4ceaa92590511845b1742eec7dcfe1
Victor <victorzamoragutierrez@gmail.com>
2017-03-20 00:41:03 -0600

    Commit inicial iguess

---
+++ SuffixTree.java
@@ -0,0 +1,7 @@
+/* Clase para árboles de sufijo. 
+ * A lo largo de este archivo, voy a suponer que '$' no está en el alfabeto.
+ * '$' será utilizado para representar el final de una cadena. */
+public class SuffixTree{
+
+    
+}
