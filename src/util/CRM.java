e78067960a5aa76d436f63a581eca9da686f4d81
Víctor <victor.z@ciencias.unam.mx>
2017-09-08 19:22:54 -0500

    Casi terminadas CRM

---
+++ src/util/CRM.java
@@ -0,0 +1,58 @@
+/* Clase con que calcula consultas de rango mínimo en un arreglo */
+
+package util;
+
+/**
+ * Clase para realizar consultas de rango mínimo en un arreglo.
+ * @author Víctor Zamora Gutiérrez.
+ */
+public class CRM{
+
+    private int[] arreglo; /* Arreglo sobre el que realizaremos la consulta */ 
+    private Integer[][] query; /* Arreglo sobre el que haremos las consultas */
+    
+    /**
+     * Constructor que realiza el preprocesamiento para llevar a cabo consultas de rango mínimo. 
+     * @param arreglo - El arreglo sobre el que realizaremos las consultas.
+     */
+    public CRM(int[] arreglo){
+	this.arreglo = arreglo;
+	System.out.println(Math.log(arreglo.length)/Math.log(2));
+	this.query = new Integer[arreglo.length][(int)(Math.log(arreglo.length))+1];
+    }
+    
+    /**
+     * Devuelve el índice con el mínimo valor entre dos índices.
+     * @param i - El extremo izquierdo del rango
+     * @param d - El extremo derecho del rango
+     * @return Regresa el índice con el mismo valor entre los dos parámetros
+     */
+    public int Consulta(int i, int d){
+	int k = (int)Math.log(d-i); /* Tamaño de bloque a revisar */
+	int indice1 = getMin(i, k); /* Índice del bloque izquierdo */
+	int indice2 = getMin((int)(d-Math.pow(2, k)+1), k); /* Índice del bloque derecho */
+	int valor1 = arreglo[indice1]; /* Valor del bloque izquierdo */
+	int valor2 = arreglo[indice2]; /* Valor del bloque derecho */
+	return valor1 < valor2 ? indice1 : indice2;
+    }
+
+    /**
+     * Devuelve el índice del mínimo element en el intervalo [i, i + 2^k -1], 
+     * dentro de nuestro arreglo.
+     * @param i - Índice en el que inicia el intervalo.
+     * @param k - Logaritmo del tamaño del intervalo.
+     * @return El índice del mínimo elemento en el intervalo [i, i + 2^k -1].
+     */
+    public int getMin(int i, int k){
+	if(query[i][k] == null){
+	    if(k == 0)
+		query[i][k] = i;
+	    else{
+		int indice1 = getMin(i, k-1); /* Índice del mínimo del lado izquierdo */
+		int indice2 = getMin((int)(i+Math.pow(2, k-1)-1), k-1); /* Índice del mínimo del lado derecho */
+		query[i][k] = arreglo[indice1] <= arreglo[indice2] ? indice1 : indice2; 
+	    }
+	}
+	return query[i][k];
+    }
+} 
---
+++ src/util/test/TestCRM.java
@@ -0,0 +1,31 @@
+package util.test;
+
+import util.CRM; 
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+
+
+/**
+ * Clase para pruebas unitarias de la clase {@link CRM}.
+ */
+public class TestCRM{
+
+    /** Expiración para que ninguna prueba tarde más de 5 segundos. */
+    @Rule public Timeout expiracion = Timeout.seconds(5);
+    
+    private static int[] prueba = {8, 7, 3, 20, 2, 17, 5, 21, 11, 12};
+    CRM c = new CRM(prueba);
+    
+    /**
+     * Prueba unitaria para {@link CRM#getMin}.
+     */
+    @Test public void testGetMin(){
+	Assert.assertTrue(c.getMin(3, 2) == 4);
+	Assert.assertTrue(c.getMin(4, 3) == 4);
+	
+    }
+     
+    
+}
